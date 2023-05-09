7d5e4b8171b3088bd5a2008cd595a3fda290b464
fhasancelik <hasancelikjob@gmail.com>
2023-05-09 03:02:11 +0300

    bug fixed

--- App.js
+++ App.js
@@ -7,12 +7,7 @@ import { name as appName } from './app.json';
 import { initializeApp } from '@react-native-firebase/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCDMCiBX7NQB-uWjnvf49_WSZy3Afu7Qgg",
-  authDomain: "ecommerce-57334.firebaseapp.com",
-  projectId: "ecommerce-57334",
-  storageBucket: "ecommerce-57334.appspot.com",
-  messagingSenderId: "519606646098",
-  appId: "1:519606646098:web:aa38c419463a83a591768b"
+
 };
 
 initializeApp(firebaseConfig);
